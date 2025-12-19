61e63b26270a4c54d020ec69004d752630aa1f61
Cory Latschkowski <clatschk@redhat.com>
2025-12-18 23:17:09 -0700

    update: gitops

--- scripts/mod-2.sh
+++ scripts/mod-2.sh
@@ -4,3 +4,26 @@ rhads_get_keycloak_admin(){
   oc -n tssc-keycloak \
     extract secret/keycloak-initial-admin --to -
 }
+
+rhads_patch_argo(){
+
+cat > patch.yaml <<YAML
+spec:
+  rbac:
+    defaultPolicy: ''
+    policy: |
+      g, system:cluster-admins, role:admin
+      g, cluster-admins, role:admin
+      g, developer-hub, role:readonly
+    scopes: '[groups]'
+  extraConfig:
+    accounts.admin: 'apiKey, login'
+    accounts.developer-hub: 'apiKey, login'
+YAML
+
+  oc -n tssc-gitops \
+    patch argocd tssc-gitops \
+    --type=merge --patch-file patch.yaml
+
+  rm patch.yaml
+}
